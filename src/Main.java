e38b03726fda290b46e8182a7fe17cd2575d2a83
AndreyTerechenko <Dronka97@yandex.ru>
2024-04-07 10:11:12 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String phone = "1234567890";
+        if (phone.length() == 10) {
+            phone = "+7" + phone;
+
+        }
+    }
+}
